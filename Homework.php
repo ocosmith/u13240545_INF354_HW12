841131b15936f44159cc230923af79092810db51
Davidsobey <Davidsobey@users.noreply.github.com>
2015-05-26 18:55:08 +0200

    Create Homework.php

---
+++ Homework.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <title>Homework 12</title>
+        
+    </head>
+	<body>
+		<h1>INF 354 Homework 12</h1>
+		<?php
+		echo "<p>David Sobey 13240545</p>";
+		
+		?>
+	</body>
+</html>
+        
+        
